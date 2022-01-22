daed264bbb3d2cca56ad55e4407f48240b2f16f7
asae mejia <you@example.com>
2022-01-22 09:39:47 -0600

    first commit

---
+++ ciclo-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ciclo for basico en php</title>
+</head>
+<body>
+  <?php
+  // crear la tabla de multiplicar del 5
+$numero = 8;
+  for($i=1; $i <= 10; $i++) {
+      echo " <p>$numero x $i =  " . ($numero*$i) . "</p>";
+    }
+  ?> 
+</body
+</html>
---
+++ ciclo-3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ciclo for basico en php</title>
+</head>
+<body>
+  <?php
+// Imprimir los numeros  menores del 20 en orden inverso dentro de ciclo for 
+for ($i=19; $i >= 0; $i--){
+    echo $i . "<br>";
+}
+
+  ?>
+</body
+</html>
